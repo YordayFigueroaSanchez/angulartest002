45c30bb986e5eedff75359186573bf5515541497
yfsanchez <yfsanchez@gmail.com>
2024-10-02 22:07:03 -0300

    sacando importaciones olvidadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,6 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { NotpagefoundComponent } from './pages/notpagefound/notpagefound.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
