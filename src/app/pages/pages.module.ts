c6c5eb383750506c584f9402031eabb06eb3e378
yfsanchez <yfsanchez@gmail.com>
2024-10-08 00:29:53 -0300

    progress bar

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages/pages.component';
 import { SharedModule } from '../shared/shared.module';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
+    FormsModule,
     CommonModule,
     SharedModule,
     RouterModule
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -3,13 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-progress',
   templateUrl: './progress.component.html',
-  styles: []
+  styleUrls: ['./progress.component.css']
 })
 export class ProgressComponent implements OnInit {
 
+  progreso: number = 55;
   constructor() { }
 
   ngOnInit() {
   }
+
+  get getProgreso(){
+    // console.log('paso por getProgreso');
+    const aaa : string = `${ this.progreso }%`;
+    // console.log(aaa);
+    return aaa;
+  }
+
+  cambiarValor(cantidad : number){
+    
+    if (this.progreso >= 100 && cantidad >= 0) {
+      return this.progreso = 100;
+    }
+
+    if (this.progreso <= 0 && cantidad < 0) {
+      return this.progreso = 0;
+    }
+
+    this.progreso += cantidad;
+  }
+
+
 
 }
