3451bf7cbf7891af56e00399734f0cfd6118afb6
yfsanchez <yfsanchez@gmail.com>
2024-10-09 01:06:44 -0300

    componente incrementador

---
+++ src/app/components/incrementador/incrementador.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-incrementador',
+  templateUrl: './incrementador.component.html',
+  styles: []
+})
+export class IncrementadorComponent {
+
+  progreso: number = 55;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  get getProgreso(){
+    // console.log('paso por getProgreso');
+    const aaa : string = `${ this.progreso }%`;
+    // console.log(aaa);
+    return aaa;
+  }
+
+  cambiarValor(cantidad : number){
+    
+    if (this.progreso >= 100 && cantidad >= 0) {
+      return this.progreso = 100;
+    }
+
+    if (this.progreso <= 0 && cantidad < 0) {
+      return this.progreso = 0;
+    }
+
+    this.progreso += cantidad;
+  }
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
+import { SharedModule } from '../shared/shared.module';
+import { ComponentsModule } from '../components/components.module';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
 import { PagesComponent } from './pages/pages.component';
-import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     FormsModule,
     CommonModule,
     SharedModule,
-    RouterModule
+    RouterModule,
+    ComponentsModule
   ],
   declarations: [
     DashboardComponent,
