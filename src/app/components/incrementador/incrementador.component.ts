5f67db9599235a388af3bb92214793cfc0e993c5
yfsanchez <yfsanchez@gmail.com>
2024-10-14 00:16:46 -0300

    limite del imput

--- src/app/components/incrementador/incrementador.component.ts
+++ src/app/components/incrementador/incrementador.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-incrementador',
@@ -6,9 +6,6 @@ import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
   styles: []
 })
 export class IncrementadorComponent implements OnInit{
-  ngOnInit(): void {
-    this.btnClass = `btn ${ this.btnClass }`
-  }
 
   // @Input('valor') progreso: number = 55;
   @Input() progreso: number = 55;
@@ -16,6 +13,26 @@ export class IncrementadorComponent implements OnInit{
 
   // @Output('valor') valorSalida: EventEmitter<number> = new EventEmitter();
   @Output() valorSalida: EventEmitter<number> = new EventEmitter();
+  
+  // ngOnChanges(changes: SimpleChanges): void {
+  //   console.log(changes);
+  // }
+  
+  onChanges(nuevoValor: number): void {
+    if (nuevoValor > 100) {
+      this.progreso = 100;
+    } else if (nuevoValor < 0) {
+      this.progreso = 0
+    } else {
+      this.progreso = nuevoValor
+    }
+    console.log(nuevoValor);
+  }
+
+  ngOnInit(): void {
+    this.btnClass = `btn ${ this.btnClass }`
+  }
+
 
   cambiarValor(cantidad : number){
     
